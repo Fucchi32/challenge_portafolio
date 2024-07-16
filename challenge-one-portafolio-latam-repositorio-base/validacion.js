01f83f814785d72a475cc138d3e9cd89e0bef14f
Fucchi32 <159728941+Fucchi32@users.noreply.github.com>
2024-07-15 21:08:34 -0400

    Add files via upload

---
+++ challenge-one-portafolio-latam-repositorio-base/validacion.js
@@ -0,0 +1,16 @@
+//Haz tú validación en javascript acá
+/* const Nombre = document.querySelector('#nombre');
+const Email = document.querySelector('#email');
+const Asunto = document.querySelector('#asunto');
+const textArea = document.querySelector('.formcontato__textarea');
+const BotonForm = document.querySelector('.formcontato__boton');
+
+const campoDeFormulario = document.querySelectorAll('[required]');
+const formulario = document.querySelector('[data-formulario]'); */
+
+
+
+const enviarFormulario = () => {
+    event.preventDefault();
+    console.log('procesando');
+}
